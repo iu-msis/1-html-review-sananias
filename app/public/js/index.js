a03bbcb4f5c6c65d8d3a937f63ed1eec6cc093b3
sananias <89755110+sananias@users.noreply.github.com>
2021-09-22 10:30:56 -0400

    Commit HW4

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -12,19 +12,31 @@ computed: {
     }
 
 },    
-created () {
-    
-    fetch('https://randomuser.me/api/')
-    .then(response => response.json())
-    .then( (responseJson) =>{console.log(responseJson);
-    this.person = responseJson.results[0];
-  
-   
-    })
 
- .catch( (err) => {
-     console.error(err);
+methods: {
+    fetchUserData() {
+        console.log("A");
+        fetch('https://randomuser.me/api/')
+        .then( response => response.json() )
+        .then( (responseJson) => {
+            console.log(responseJson);
+            console.log("C");
+            this.person = responseJson.results[0];
+        })
+        .catch( (err) => {
+            console.error(err);
+        })
+          
+    .catch( (err) => {
+        console.error(err);
+       
     })
+    console.log("B");
+       }
+},
+created() {
+    this.fetchUserData();
+
 
 }//end created
 
