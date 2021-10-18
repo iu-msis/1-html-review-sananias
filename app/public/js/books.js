0eb24eaa6b6a0861dae5380503dae874d22dbde3
sananias <89755110+sananias@users.noreply.github.com>
2021-10-18 12:39:52 -0400

    Class 10/18

--- app/public/js/books.js
+++ app/public/js/books.js
@@ -1,18 +1,19 @@
 const Book = {
     data() {
       return {
-        "books": [],
+        books: [],
+        bookForm: {}
       }
     },
   
-
     methods: {
+// Currency Formatting ------------------------------------------------------------------------------------
 
       prettyDollar(n) {
         const d = new Intl.NumberFormat("en-US").format(n);
         return "$ " + d;
     },
-
+// API to SQL for Books Table ------------------------------------------------------------------------------------
         fetchBooksData() {
             fetch('/api/books/')
             .then( response => response.json() )
@@ -24,10 +25,32 @@ const Book = {
                 console.error(err);
             })
         },
-    },
+// Post New Book from form ------------------------------------------------------------------------------------
+postNewOffer(evt) { 
+  console.log("Posting!", this.bookForm);
+
+  fetch('api/books/create.php', {
+      method:'POST',
+      body: JSON.stringify(this.bookForm),
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      }
+    })
+    .then( response => response.json() )
+    .then( json => {
+      console.log("Returned from post:", json);
+      this.books = json;
+      
+      this.bookForm = {};
+    });
+}
+   
+// Closing Methods ------------------------------------------------------------------------------------
+              },
+// Created ------------------------------------------------------------------------------------
+
     created() {
         this.fetchBooksData();
     }
-  
   }
 Vue.createApp(Book).mount('#bookApp');
